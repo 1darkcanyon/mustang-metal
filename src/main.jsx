26191930599779434644dac4a2776a854cc18d18
1darkcanyon <dudeineedajob0505@gmail.com>
2026-05-27 22:55:46 -0700

    Fix import statement for index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.jsx'
-import './index.css' // <-- THIS LINE LINKS THE NEON STYLES
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
